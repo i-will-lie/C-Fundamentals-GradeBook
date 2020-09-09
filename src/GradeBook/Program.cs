96dd1b9d092542db483e8f08b407ab08323d42b2
i-will-lie <i-will-lie@users.noreply.github.com>
2020-09-09 20:10:20 +1200

    Initial Commit
    Upto chapter 3.1.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 34.1;
+            var y = 10.3;
+ 
+
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine($"Hello!");
+            }
+        }
+    }
+}
